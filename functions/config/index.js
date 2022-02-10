e9f34ddaf378883ab29e3d763f662b5394e8fa84
Anitor <anitorabraham@gmail.com>
2022-02-10 23:28:07 +0100

    working folder

--- functions/config/index.js
+++ functions/config/index.js
@@ -1,5 +1,5 @@
-const BASE_URL = "https://evening-mountain-86930.herokuapp.com/";
-// const BASE_URL = "http://localhost:4000/api/v1/";
+// const BASE_URL = "https://evening-mountain-86930.herokuapp.com/";
+const BASE_URL = "http://localhost:4000/api/v1/";
 
 const KEYS = {
     token: "yuduis787878755tokk88",
